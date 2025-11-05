e01692f2e13acb5655bd271de652ca2d11fc658c
nuriagarcias <nuriagarcias@alumnat.iesdamiahuguet.net>
2025-11-05 09:42:47 +0100

    cistella pràctica feta

--- funcionstenda.js
+++ funcionstenda.js
@@ -1,1 +1,15 @@
-let diners= 0
+let nombreProductes = 0
+
+function Comprar(nom,preu){
+    document.getElementById('cistella').innerText += nom + " - " + preu +"€\n"
+    nombreProductes++
+    document.getElementById('comptadorProductes').innerText = nombreProductes + " articles"
+}
+
+let cost=0
+
+function Sumar(preu){
+    cost = cost + preu;
+    document.getElementById("CampPreu").innerText="Preu de la compra: " + Number(cost).toFixed(2);     // 1.34
+    ;
+}
